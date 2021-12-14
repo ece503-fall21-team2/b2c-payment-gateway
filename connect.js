2da50c46368f0d14b2c711b48b125ceee484b6a2
Khyati <92055071+khyati-dp@users.noreply.github.com>
2021-12-13 19:05:59 -0500

    Updated connect.js file

--- connect.js
+++ connect.js
@@ -2,10 +2,10 @@ const { Pool } = require('pg');
 
 const pool = new Pool({
   user: 'postgres',
-  password: 'Khyshu#12345',
+  password: 'Khyshu', #Enter passsword for your databse
   host: 'localhost',
   port: 5432,
-  database: 'b2c_payment'
+  database: 'b2c_payment' #Enter your database name
 });
 
 const getClient = async () => {
